d1ac0694019c5f0babce7e9a543fea137d26f619
BuildTools <unconfigured@null.spigotmc.org>
2024-07-01 19:46:15 -0400

    Yok Tural maps added

---
+++ fishmap/Urqopacha/generateThisMap.m
@@ -0,0 +1,10 @@
+mapSettings.legendX = 240;
+mapSettings.legendY = 240;
+mapSettings.legendW = 875;
+mapSettings.legendBox = true;
+mapSettings.enable0 = false;
+mapSettings.specialLayer = [];
+mapSettings.skip = zeros(1, 4);
+mapSettings.highlight = false;
+
+mapGenerator(mapSettings)
