7ef79f4c0d66a5fd5de2a9972786a9069cf4c1d9
sinjidk <170571194+sinjidk@users.noreply.github.com>
2026-06-03 16:41:39 -0400

    Add Elysion

---
+++ fishmap/Elysion/generateThisMap.m
@@ -0,0 +1,11 @@
+mapSettings.legendX = 200;
+mapSettings.legendY = 225;
+mapSettings.legendW = 800;
+mapSettings.legendBox = true;
+mapSettings.enable0 = false;
+mapSettings.specialLayer = [];
+mapSettings.skip = zeros(1, 6);
+mapSettings.highlight = false;
+mapSettings.makeAlts = true;
+
+mapGenerator(mapSettings)
