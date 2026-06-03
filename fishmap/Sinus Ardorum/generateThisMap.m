fac44696e81a21634a3052af9f96fc2b14e52c01
sinjidk <170571194+sinjidk@users.noreply.github.com>
2026-06-03 11:36:24 -0400

    add Sinus Ardorum

---
+++ fishmap/Sinus Ardorum/generateThisMap.m
@@ -0,0 +1,11 @@
+mapSettings.legendX = 1275;
+mapSettings.legendY = 700;
+mapSettings.legendW = 740;
+mapSettings.legendBox = true;
+mapSettings.enable0 = false;
+mapSettings.specialLayer = [];
+mapSettings.skip = zeros(1, 10);
+mapSettings.highlight = false;
+mapSettings.makeAlts = true;
+
+mapGenerator(mapSettings)
