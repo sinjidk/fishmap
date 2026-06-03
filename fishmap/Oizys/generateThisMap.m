a15f4d83cfb5929235c209a9ce248b42d64fe7a2
sinjidk <170571194+sinjidk@users.noreply.github.com>
2026-06-02 23:11:35 -0400

    Added Oizys

---
+++ fishmap/Oizys/generateThisMap.m
@@ -0,0 +1,11 @@
+mapSettings.legendX = 80;
+mapSettings.legendY = 5;
+mapSettings.legendW = 650;
+mapSettings.legendBox = true;
+mapSettings.enable0 = false;
+mapSettings.specialLayer = [];
+mapSettings.skip = zeros(1, 8);
+mapSettings.highlight = false;
+mapSettings.makeAlts = true;
+
+mapGenerator(mapSettings)
