21d50b401e23f93e015b1a4083a6c478bbd7a1d0
sinjidk <170571194+sinjidk@users.noreply.github.com>
2026-06-09 23:36:04 -0400

    temporarily remove folders from project

---
+++ fishmap/Yak T'el/generateThisMap.m
@@ -0,0 +1,10 @@
+mapSettings.legendX = 100;
+mapSettings.legendY = 1050;
+mapSettings.legendW = 900;
+mapSettings.legendBox = false;
+mapSettings.enable0 = false;
+mapSettings.specialLayer = [];
+mapSettings.skip = [-5 -5 -5 -5 -5 -5 0 0 0 0 0 0 0];
+mapSettings.highlight = true;
+
+mapGenerator(mapSettings)
